bced4c6dc8f72d56e49ecbf1b24b91019b0efc34
ocha <ochaochaocha3@gmail.com>
2015-10-12 00:12:52 +0900

    Rails 動作に必要なパッケージを追加する

--- spec/refugeerpi2/rails_spec.rb
+++
@@ -1,4 +0,0 @@
-# Rails のバージョンが 4.2.x
-describe command('/usr/local/rbenv/shims/rails --version') do
-  its(:stdout) { should match(/^Rails 4\.2/) }
-end
---
+++ spec/refugeerpi2/web_app_spec.rb
@@ -0,0 +1,12 @@
+packages = %w(nginx firebird2.5-superclassic firebird-dev nodejs)
+
+packages.each do |p|
+  describe package(p) do
+    it { should be_installed }
+  end
+end
+
+# Rails のバージョンが 4.2.x
+describe command('/usr/local/rbenv/shims/rails --version') do
+  its(:stdout) { should match(/^Rails 4\.2/) }
+end
