3a764db134a6ec599354ebb265b710f28ba79481
Kosuke Yamashita <ochaochaocha3@gmail.com>
2016-01-08 20:24:54 +0900

    Web アプリの実行に必要なサービスについて記述する

--- spec/refugeerpi2/web_app_spec.rb
+++ spec/refugeerpi2/web_app_spec.rb
@@ -1,10 +1,17 @@
 # Web アプリに関する spec
 
 packages = %w(nginx firebird2.5-superclassic firebird-dev nodejs)
+services = %w(refugee-webapp refugee-input-server refugee-com-server)
 
 packages.each do |p|
   describe package(p) do
     it { should be_installed }
+  end
+end
+
+services.each do |s|
+  describe service(s) do
+    it { should be_enabled }
   end
 end
 
