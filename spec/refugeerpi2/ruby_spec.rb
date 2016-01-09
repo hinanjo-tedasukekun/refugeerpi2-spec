b43ddd85db2283d4e0f10e362bf54410856ae5da
ocha <ochaochaocha3@gmail.com>
2016-01-10 08:32:01 +0900

    Ruby 用の設定ファイルの確認を追加する

--- spec/refugeerpi2/ruby_spec.rb
+++ spec/refugeerpi2/ruby_spec.rb
@@ -24,6 +24,14 @@ describe file('/usr/local/rbenv/bin/rbenv') do
   it { should be_executable }
 end
 
+describe file('/etc/profile.d/rbenv.sh') do
+  it { should be_exist }
+end
+
+describe file('/usr/local/rbenv/default-gems') do
+  it { should be_exist }
+end
+
 # Ruby のバージョンが 2.3.0
 describe command('/usr/local/rbenv/shims/ruby --version') do
   its(:stdout) { should match(/^ruby 2\.3\.0/) }
