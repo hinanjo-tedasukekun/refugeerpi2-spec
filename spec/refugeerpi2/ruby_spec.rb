74828218e8be04e3d3bc62fc8349d0639950b33d
ocha <ochaochaocha3@gmail.com>
2016-01-09 13:50:17 +0900

    Ruby のビルドに必要なものを追加する

--- spec/refugeerpi2/ruby_spec.rb
+++ spec/refugeerpi2/ruby_spec.rb
@@ -1,6 +1,30 @@
 # Ruby に関する spec
 
-# Ruby のバージョンが 2.2.3
+packages = %w(
+autoconf
+bison
+build-essential
+libssl-dev
+libyaml-dev
+libreadline6-dev
+zlib1g-dev
+libncurses5-dev
+libffi-dev
+libgdbm3
+libgdbm-dev
+)
+
+packages.each do |pkg|
+  describe package(pkg) do
+    it { should be_installed }
+  end
+end
+
+describe file('/usr/local/rbenv/bin/rbenv') do
+  it { should be_executable }
+end
+
+# Ruby のバージョンが 2.3.0
 describe command('/usr/local/rbenv/shims/ruby --version') do
-  its(:stdout) { should match(/^ruby 2\.2\.3/) }
+  its(:stdout) { should match(/^ruby 2\.3\.0/) }
 end
