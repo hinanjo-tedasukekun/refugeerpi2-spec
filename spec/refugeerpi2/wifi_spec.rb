6871facfc4ba949dc518efdd140ec5bd6fd6a521
ocha <ochaochaocha3@gmail.com>
2016-01-09 13:58:18 +0900

    パッケージとして提供される hostapd を使わないようにする

--- spec/refugeerpi2/wifi_spec.rb
+++ spec/refugeerpi2/wifi_spec.rb
@@ -2,12 +2,22 @@
 
 require 'spec_helper'
 
-packages = %w(hostapd dnsmasq iw)
+packages = %w(
+libnl-3-200
+libnl-genl-3-200
+libnl-route-3-200
+libssl1.0.0
+dnsmasq
+iw)
 
 packages.each do |p|
   describe package(p) do
     it { should be_installed }
   end
+end
+
+describe file('/usr/local/sbin/hostapd') do
+  it { should be_executable }
 end
 
 describe host('hinan') do
